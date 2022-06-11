4fc970d53f115bc5d2c512e7e31e9684b357e83e
Danil <danil.polovinkin.1994@gmail.com>
2022-06-11 10:36:15 +0300

    v 1

---
+++ src/scripts/main.js
@@ -0,0 +1,23 @@
+
+ import Swiper, { Navigation, Pagination } from 'swiper';
+
+
+ const swiper = new Swiper('.swiper', {
+	spaceBetween: 1,
+    slidesPerView: 1,
+    centeredSlides: true,
+    roundLengths: true,
+    loop: true,
+    loopAdditionalSlides: 30,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    },
+	modules: [Navigation, Pagination],
+  });
+
+  document.querySelectorAll("button").forEach(item => {
+	  item.addEventListener("click" , e => {
+		  e.preventDefault();
+	  })
+  })
